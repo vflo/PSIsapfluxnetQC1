d1981fe0d87371ff480f40cea221f55601d35a30
vflo <vflosierra@gmail.com>
2021-06-23 13:20:56 +0200

    main script improved

--- inst/run_scripts/main_script.R
+++ inst/run_scripts/main_script.R
@@ -3,42 +3,20 @@
 library(psiQC)
 library(tidyverse)
 
+################################################################################
+# # LEVEL 1
 
-# setup logs
-log_psi_setup('Logs/psi.log',
-              logger = 'DataFlow',
-              level = 'DEBUG')
+lvl1_process()
 
-# reports for data in the system
-rep_psi_render('received_to_accepted.Rmd',
-               output_file = file.path(
-                 'Reports', paste(format(Sys.time(), '%Y%m%d%H%M'),
-                                  'received_to_accepted.html', sep = '_')
-               ),
-               output_dir = 'Reports',
-               parent_logger = 'DataFlow')
-
-# QC
-log_psi_setup('Logs/psi.log', logger = 'QC', level = "DEBUG")
+## Manually set site prepared for level 2
 
 data_folders <- df_get_data_folders(parent_logger = 'QC')
 
-## Loop for every site
-lapply(data_folders, function(folder) {
-  code <- stringr::str_sub(folder, 6, -1)
-  # log_psi_setup('Logs/psi.log',
-  #                      logger = paste('QC', code, sep = '.'),
-  #                      level = "DEBUG")
-  qc_start_process_psi(file.path(folder, 'Accepted'), rdata = FALSE,
-                       parent_logger = paste('QC', code, sep = '.'))
-})
-
-## Site prepared for level 2
 df_set_status_psi(data_folders[[12]] %>% stringr::str_sub( 6, -1),
                   LVL1 = list(TO_LVL2 = "READY"))
 
 ## Reset status of a site (RUN ONLY when changes are applied to received data)
-# df_reset_data_status_psi(data_folders[[12]] %>% stringr::str_sub( 6, -1))
+#df_reset_data_status_psi(data_folders[[1]] %>% stringr::str_sub( 6, -1))
 
 ################################################################################
 # # LEVEL 2
@@ -53,6 +31,8 @@ lvl2_process()
 
 
 ################################################################################
+## CREATE A JOINED DATA SET
+
 folder <- "~/psi_db/0.0.1/RData/"
 
 psi_files <- list.files(folder) %>% gsub(".RData","",x=.)
